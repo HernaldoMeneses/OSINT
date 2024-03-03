14c5132407ed35217044f745cb7f259285e93f87
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 20:11:04 -0300

    1.1-internet.py

---
+++ 1.1-internet.py
@@ -0,0 +1,9 @@
+#OSINT - Open souce Inteligence Techniques
+
+#The internet as a powerfull way of get information
+#Even Hacking conferences proudly host OSINT traning session.
+#Law enforcement agencies dedicate entire investigation units to online reserch into criminals
+#Journalist rely on the methods every day
+#Social engineering crowd has adopted it as part of their playbook
+
+OSINT = []
