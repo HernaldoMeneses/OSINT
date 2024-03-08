c0d20f39dc4750d39ea87d7eed5cd41f42959a28
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-08 08:51:15 -0300

    1.1-internet.py up

--- 1.1-internet.py
+++ 1.1-internet.py
@@ -6,6 +6,19 @@
 #Journalist rely on the methods every day
 #Social engineering crowd has adopted it as part of their playbook
 
-OSINT = []
+OSINT = [
+    '''
+    #Any intelligence produced from publicly vailable information that is 
+    #collected, exploited, and disseminated 
+    #in a timely manner to an appropriate audience for the purpose 
+    #of addressing aspecifc intelligence requirement.'
+''',
+    '''
+    #Appropreate collection and reporting 
+    #documentation is need,, information is not being available online forever.
+''',
+
+]
 
 #pg. 8/526
+#file:///L:/Linear_/Base-Jump_06/Open%20Source%20Intelligence%20Techniques%209%20edition_bibis.ir.pdf
---
+++ 1.2-book-audience.py
@@ -0,0 +1,2 @@
+#pg 9/526
+#file:///L:/Linear_/Base-Jump_06/Open%20Source%20Intelligence%20Techniques%209%20edition_bibis.ir.pdf
