a393d03251413b3f269cd6472bd8965cb23d9658
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-04-24 11:28:12 -0300

    Update 1.1-Optimization.py

---
+++ 1-Computer/1.1-Optimization.py
@@ -0,0 +1,10 @@
+"""
+    What data is on your computer?
+    Is there a virus, malicious software, or spyware hanging around from casual browsing in wuestionable places?
+    Does your internet cache include tracking cookies from Amazon, Facebook, Google, and others?
+    Is there evidence of your last investigation stored within your bookmarks, documents, or download queue?
+
+    If the answer is "maybe" or "I don't know" to any of these,
+
+            You have a contaminated computer.
+"""
---
+++ 1-OSINT/1.1-Preparation.py
@@ -0,0 +1,7 @@
+#This entire section explains the essential 
+#Steps which I believe any online investigator should 
+#Complete before ever conducting a search online. 
+# 
+# Much like a DNA forensics lab 
+#  must be configured and ready before an investigation
+#    your OSINT lab should face the same secutiny
